4bfcccc1cff49f1c4343c03f1f60c28b958b8f49
Konstantin Zagorulya <k.zagorulya@mail.ru>
2023-09-25 14:47:37 +0600

    Update ckass Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,12 +9,14 @@ public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
         UserServiceImpl userService = new UserServiceImpl();
+
         userService.createUsersTable();
         userService.saveUser("Petr", "Petrov", (byte) 23);
         userService.saveUser("Alex", "Sidorov", (byte) 25);
         userService.saveUser("Anna", "K", (byte) 21);
         userService.saveUser("Vasilii", "Petrov", (byte) 20);
 
+        userService.getAllUsers();
         userService.cleanUsersTable();
         userService.dropUsersTable();
     }
